594eeecfb747a34d11b31a829df916df9a516275
Matheus Cassioli <matheuscassioli@hotmail.com>
2023-01-31 09:49:31 -0300

    corrigindo falhas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./Styles/globalStyles.scss"
-import Header from "Componentes/Header"
-import Menu from "Componentes/Menu";
-import Banner from "Componentes/Banner";
-import Rodape from "Componentes/Rodape";
-import Galeria from "Componentes/Galeria";
-import MenuMobile from "Componentes/MenuMobile";
+import Header from "./Componentes/Header"
+import Menu from "./Componentes/Menu";
+import Banner from "./Componentes/Banner";
+import Rodape from "./Componentes/Rodape";
+import Galeria from "./Componentes/Galeria";
+import MenuMobile from "./Componentes/MenuMobile";
 
 export default function App() {
   return (
